9a554cb13d737b6e9fe666b31be22a9fbbaa8492
김성섭 <tjdtjq91@gmail.com>
2023-11-15 18:54:09 +0900

    Fix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,9 +8,11 @@ alias .3='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../../'
 
+# navigation aliases
 alias dl="cd ~/Downloads"
 alias desk="cd ~/Desktop"
 alias dot="cd ~/.dotfiles"
+alias dev="cd ~/dev"
 
 alias vim="nvim"
 alias vi="nvim"
@@ -29,8 +31,8 @@ alias mv='mv -i'
 alias update="brew update && brew upgrade && brew cleanup"
 alias services="brew services"
 
-alias ll="eza --color-scale --icons --time-style long-iso -lhbg"
-alias la="eza --color-scale --icons --time-style long-iso -lahbg"
+alias ll="eza --color-scale all --icons --time-style long-iso -lhbg"
+alias la="eza --color-scale all --icons --time-style long-iso -lahbg"
 alias ls="eza"
 alias lt="eza --tree --level=2"
 
@@ -44,4 +46,16 @@ alias d='date +%F'
 alias now='date +"%T"'
 alias nowtime=now
 alias nowdate='date +"%Y-%m-%d"'
+
+# ping: stop after 5 pings
+alias ping='ping -c 5'
+
+# curl: only display HTTP header
+alias header='curl -I'
+
+## set some other defaults ##
+alias df='duf'
+alias du='du -ch'
+
+alias ag="alias | grep " # +command
 
