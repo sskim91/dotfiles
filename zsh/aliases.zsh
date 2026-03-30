26faaf05bbfe789d7b65620b4c44577b383612ba
sskim91 <tjdtjq91@gmail.com>
2026-03-30 22:51:54 +0900

    Add oss directory shortcut alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,6 +16,7 @@ alias dot="cd ~/.dotfiles"
 alias dev="cd ~/dev"
 alias til="cd ~/dev/TIL"
 alias comp="cd ~/company-src"
+alias oss="cd ~/dev/oss"
 alias note="cd ~/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Note"
 
 # 향상된 도구로 대체
